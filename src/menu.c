5f205d8e51bc819f3a26e56c8ee1ff5272f7a28f
lmartinking <lmartinking@gmail.com>
2006-08-13 10:15:25 +0000

    * Code cleanup
    git-svn-id: svn://svn.icculus.org/cdogs-sdl/trunk@58 d5de7276-5e10-0410-86d4-898530c01de9

--- src/menu.c
+++ src/menu.c
@@ -32,8 +32,7 @@
 
 void ShowControls(void)
 {
-	TextStringAt(50, 190,
-		     "(use player 1 controls or arrow keys + Enter/Backspace)");
+	TextStringAt(50, 190, "(use player 1 controls or arrow keys + Enter/Backspace)");
 }
 
 void DisplayMenuItem(int x, int y, const char *s, int selected)
@@ -42,13 +41,19 @@ void DisplayMenuItem(int x, int y, const char *s, int selected)
 		TextStringWithTableAt(x, y, s, tableFlamed);
 	else
 		TextStringAt(x, y, s);
+		
+	return;
 }
+
+#define MENU_OFFSET_Y	50
 
 void DisplayMenu(int x, const char **table, int count, int index)
 {
 	int i;
 
-	for (i = 0; i < count; i++)
-		DisplayMenuItem(x, 50 + i * TextHeight(), table[i],
-				i == index);
+	for (i = 0; i < count; i++) {
+		DisplayMenuItem(x, MENU_OFFSET_Y + i * TextHeight(), table[i], i == index);
+	}
+	
+	return;
 }
