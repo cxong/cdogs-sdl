9b0099c903622205a2358353303a67e5831b73d3
lmartinking <lmartinking@gmail.com>
2007-07-06 12:07:44 +0000

    Remove legacy stuff which isn't used anymore.
    git-svn-id: svn://svn.icculus.org/cdogs-sdl/trunk@243 d5de7276-5e10-0410-86d4-898530c01de9

--- src/keyboard.c
+++ src/keyboard.c
@@ -36,50 +36,6 @@
 #include "SDL.h"
 #include "keyboard.h"
 
-char *keyNames[256] =
-    { /* $00 */ NULL, "Esc", "1", "2", "3", "4", "5", "6",
-	/* $08 */ "7", "8", "9", "0", "+", "Apostrophe", "Backspace",
-	"Tab",
-	/* $10 */ "Q", "W", "E", "R", "T", "Y", "U", "I",
-	/* $18 */ "O", "P", "è", "^", "Enter", "Left Ctrl", "A", "S",
-	/* $20 */ "D", "F", "G", "H", "J", "K", "L", "ô",
-	/* $28 */ "é", "Paragraph", "Left shift", "'", "Z", "X", "C", "V",
-	/* $30 */ "B", "N", "M", ",", ".", "-", "Right shift", "* (pad)",
-	/* $38 */ "Alt", "Space", "Caps Lock", "F1", "F2", "F3", "F4",
-	"F5",
-	/* $40 */ "F6", "F7", "F8", "F9", "F10", "Num Lock", "Scroll Lock",
-	"7 (pad)",
-	/* $48 */ "8 (pad)", "9 (pad)", "- (pad)", "4 (pad)", "5 (pad)",
-	"6 (pad)", "+ (pad)", "1 (pad)",
-	/* $50 */ "2 (pad)", "3 (pad)", "0 (pad)", ", (pad)", "SysRq",
-	NULL, "<", "F11", "F12",
-	/* $59 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	/* $60 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	/* $70 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	/* $80 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	/* $90 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	NULL, NULL, NULL, "Enter (pad)", "Right Ctrl", NULL, NULL,
-	/* $A0 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	/* $B0 */ NULL, NULL, NULL, NULL, NULL, "/ (pad)", NULL, "PrtScr",
-	"Alt Gr", NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	/* $C0 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL, "Home",
-	/* $C8 */ "Up arrow", "Page Up", NULL, "Left arrow", NULL,
-	"Right arrow", NULL, "End",
-	/* $D0 */ "Down arrow", "Page Down", "Insert", "Delete", NULL,
-	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	NULL,
-	/* $E0 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	/* $F0 */ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	NULL, NULL, NULL, NULL, NULL, NULL, NULL
-};
-
-
-static volatile unsigned char uKeys[256];
 
 void InitKeyboard(void)
 {
