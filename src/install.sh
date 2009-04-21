4bcfccf0b6e6a8643f5c568c90e9b012f52bf41b
lmartinking <lmartinking@gmail.com>
2009-04-21 12:22:11 +0000

    * cleanups and fixes
    git-svn-id: svn://svn.icculus.org/cdogs-sdl/trunk@284 d5de7276-5e10-0410-86d4-898530c01de9

--- src/install.sh
+++ src/install.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 function wantset () {
 	local var=$1
 
 	eval "[ -z \"\$$var\" ] && echo \"$var needs to be set!\" && exit 1"
+}
+
+function die () {
+	echo "$1"
+	exit 1
 }
 
 wantset DESTDIR
@@ -14,29 +19,25 @@ wantset PROG
 wantset LOCALDATA
 wantset LOCALDOCS
 
-[ ! -d $LOCALDATA ]	&& echo "$LOCALDATA not a directory!" && exit 1
-[ ! -d $LOCALDOCS ]	&& echo "$LOCALDOCS not a directory!" && exit 1
-[ ! -f $PROG ]		&& echo "$PROG not a file!" && exit 1 
+[ ! -d $LOCALDATA ]	&& die "$LOCALDATA not a directory!"
+[ ! -d $LOCALDOCS ]	&& die "$LOCALDOCS not a directory!"
+[ ! -f $PROG ]		&& die "$PROG not a file!"
 
 echo "### Installing into $DESTDIR ###"
 echo "# Data dir: ${DATADIR}"
 echo "# Docs dir: ${DOCDIR}"
 echo "# Bin dir:  ${BINDIR}" 
 
+echo "** Installing binary **"
+binpath="${DESTDIR}/${BINDIR}/"
+mkdir -p "${binpath}" && cp ${PROG} "${binpath}/${PROG}" || die "Couldn't install binary!"
+
 echo "** Installing data **"
-datapath="${DESTDIR}/${DATADIR}"
-mkdir -p "${datapath}"
-cp -R "${LOCALDATA}" "${datapath}/"
+datapath="${DESTDIR}/${DATADIR}/"
+mkdir -p "${datapath}" && cp -R ${LOCALDATA}/* "${datapath}/" || die "Couldn't install data!"
 
 echo "** Installing docs **"
 docpath="${DESTDIR}/${DOCDIR}/"
-mkdir -p "${docpath}"
-cp -R "${LOCALDOCS}" "${docpath}"
-
-echo "** Installing binary **"
-binpath="${DESTDIR}/${BINDIR}/"
-mkdir -p "${binpath}"
-cp "${PROG}" "${binpath}/${PROG}"
-chmod +x "${binpath}/${PROG}"
+mkdir -p ${docpath} && cp -R ${LOCALDOCS}/* "${docpath}/" || die "Couldn't install docs!"
 
 echo "** Finished **"
