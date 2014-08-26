50584f4eb060defcc35bd4438eab7f02036c069e
Cong <congusbongus@gmail.com>
2014-08-27 08:31:25 +1000

    Disable loading music for GCW0 (#292)

--- src/cdogs/gamedata.c
+++ src/cdogs/gamedata.c
@@ -292,10 +292,12 @@ void FreeSongs(struct SongDef **songList)
 
 void LoadSongs(void)
 {
+#ifndef __GCWZERO__
 	debug(D_NORMAL, "loading game music %s\n", CDOGS_GAME_MUSIC_DIR);
 	LoadSongList(&gGameSongs, CDOGS_GAME_MUSIC_DIR);
 	debug(D_NORMAL, "loading menu music %s\n", CDOGS_MENU_MUSIC_DIR);
 	LoadSongList(&gMenuSongs, CDOGS_MENU_MUSIC_DIR);
+#endif
 }
 
 void LoadSongList(struct SongDef **songList, const char *dirPath)
