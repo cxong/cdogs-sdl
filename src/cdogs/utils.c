99f1b2a1403b22d7e0f87e75e6a17c382b40d0fc
Cong <congusbongus@gmail.com>
2015-03-05 23:43:23 +1100

    Fix deprecated _BSD_SOURCE error (fixes #334)
    Also include #define _DEFAULT_SOURCE according to man page

--- src/cdogs/utils.c
+++ src/cdogs/utils.c
@@ -22,7 +22,7 @@
     This file incorporates work covered by the following copyright and
     permission notice:
 
-    Copyright (c) 2013-2014, Cong Xu
+    Copyright (c) 2013-2015, Cong Xu
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -46,6 +46,7 @@
     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
     POSSIBILITY OF SUCH DAMAGE.
 */
+#define _DEFAULT_SOURCE
 #define _BSD_SOURCE
 #include "utils.h"
 
