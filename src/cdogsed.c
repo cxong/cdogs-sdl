9f2a301e2ed16c203dc0c6a233800bdd70b1bf97
Cong <congusbongus@gmail.com>
2015-05-10 18:53:34 +1000

    Don't show non-campaigns in open file hint

--- src/cdogsed.c
+++ src/cdogsed.c
@@ -498,7 +498,15 @@ static void Open(void)
 				tinydir_readfile_n(&dir, &file, i);
 				if (strncmp(file.name, basename, strlen(basename)) == 0)
 				{
-					FontStrMask(file.path, pos, colorGray);
+					// Ignore files that aren't campaigns
+					if (file.name[0] == '.') continue;
+					if (!file.is_dir &&
+						strcmp(file.extension, "cdogscpn") != 0 &&
+						strcmp(file.extension, "CDOGSCPN") != 0 &&
+						strcmp(file.extension, "cpn") != 0 &&
+						strcmp(file.extension, "CPN") != 0)
+						continue;
+					pos = FontStrMask(file.path, pos, colorGray);
 					pos.x = x;
 					pos.y += FontH();
 				}
