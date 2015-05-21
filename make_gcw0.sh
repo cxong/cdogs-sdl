c221cbfb08d7332606ee471cf96a9c239a62e7c9
Cong <congusbongus@gmail.com>
2015-05-21 10:06:06 +1000

    rm build dir in make_gcw0.h

--- make_gcw0.sh
+++ make_gcw0.sh
@@ -8,6 +8,7 @@ git submodule update --init --recursive
 git submodule update --recursive
 
 # For more info: http://github.com/cxong/cdogs-sdl/wiki/Developer-Getting-Started:-GCW-Zero
+rm gcw0build -rf
 mkdir gcw0build
 cd gcw0build
 cmake -DCMAKE_TOOLCHAIN_FILE="/opt/gcw0-toolchain/usr/share/buildroot/toolchainfile.cmake" ..
