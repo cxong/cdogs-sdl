ac43de624b217b713183dab2e9a14b7c1bcaf350
Cong <congusbongus@gmail.com>
2015-10-24 14:25:51 +1100

    Properly cache travis-ci (#169)

---
+++ build/travis-ci/install-sdl2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+export SDL_PREFIX=$HOME/sdl2
+# check to see if sdl2 folder is empty
+if [ ! -d "$SDL_PREFIX" ]; then
+  curl -O http://www.libsdl.org/release/SDL2-2.0.3.tar.gz
+  tar -xzvf SDL2-2.0.3.tar.gz
+  (cd SDL2-2.0.3 && ./configure --prefix=$SDL_PREFIX && make && make install)
+  curl -O https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.0.tar.gz
+  tar -xzvf SDL2_image-2.0.0.tar.gz
+  (cd SDL2_image-2.0.0 && ./configure --prefix=$SDL_PREFIX && make && make install)
+  curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz
+  tar -xzvf SDL2_mixer-2.0.0.tar.gz
+  (cd SDL2_mixer-2.0.0 && ./configure --prefix=$SDL_PREFIX && make && make install)
+else
+  echo "Using cached directory."
+fi
