c255453c044e6f7b01c245bd97f90bc4c033b914
Cong <congusbongus@gmail.com>
2015-10-26 10:38:52 +1100

    Remove submodules from make.sh (#275)

--- make.sh
+++ make.sh
@@ -2,11 +2,6 @@
 
 # To create a debug build, run `cmake -D CMAKE_BUILD_TYPE=Debug .` instead
 
-# Update repo properly (should be handled by most git GUI clients)
-git submodule init
-git submodule update --init --recursive
-git submodule update --recursive
-
 UNAME_SHORT=`uname -s | cut -c1-5`
 if [ "$UNAME_SHORT" = "MINGW" ] ; then
 	cmake -G"MinGW Makefiles" .
