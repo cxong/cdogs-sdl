9ef327084bda85a0bd05bc261087d8a8bf551317
Cong <congusbongus@gmail.com>
2015-10-26 10:39:14 +1100

    Remove submodules from make_gcw0.sh (#275)

--- make_gcw0.sh
+++ make_gcw0.sh
@@ -2,11 +2,6 @@
 
 # To create a debug build, run `cmake -D CMAKE_BUILD_TYPE=Debug .` instead
 
-# Update repo properly (should be handled by most git GUI clients)
-git submodule init
-git submodule update --init --recursive
-git submodule update --recursive
-
 # For more info: http://github.com/cxong/cdogs-sdl/wiki/Developer-Getting-Started:-GCW-Zero
 rm CMakeCache.txt
 rm gcw0build -rf
