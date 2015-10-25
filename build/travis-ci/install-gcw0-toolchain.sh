1ad85ef7c034a5cc0fd350adc0dd508d7de66349
Cong <congusbongus@gmail.com>
2015-10-25 18:38:00 +1100

    Fix travis config (#375)

--- build/travis-ci/install-gcw0-toolchain.sh
+++ build/travis-ci/install-gcw0-toolchain.sh
@@ -3,7 +3,7 @@ set -e
 # check to see if toolchain folder is empty
 if [ ! -d "/opt/gcw0-toolchain" ]; then
   curl -O http://www.gcw-zero.com/files/opendingux-gcw0-toolchain.2014-08-20.tar.bz2
-  (sudo mkdir -p /opt && tar jxf opendingux-gcw0-toolchain.2014-08-20.tar.bz2-C /opt)
+  (mkdir -p /opt && tar jxf opendingux-gcw0-toolchain.2014-08-20.tar.bz2-C /opt)
 else
   echo "Using cached toolchain directory."
 fi
