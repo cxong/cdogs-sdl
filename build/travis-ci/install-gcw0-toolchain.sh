c4fe23884ce34cf28ae63cd59c686aa1fda92608
Cong <congusbongus@gmail.com>
2016-02-09 22:53:34 +1100

    Attempt to revert to old sudo travis build

--- build/travis-ci/install-gcw0-toolchain.sh
+++ build/travis-ci/install-gcw0-toolchain.sh
@@ -3,7 +3,7 @@ set -e
 # check to see if toolchain folder is empty
 if [ ! -d "/opt/gcw0-toolchain" ]; then
   curl -O http://www.gcw-zero.com/files/opendingux-gcw0-toolchain.2014-08-20.tar.bz2
-  (mkdir -p /opt && tar jxf opendingux-gcw0-toolchain.2014-08-20.tar.bz2-C /opt)
+  (sudo mkdir -p /opt && sudo tar jxf opendingux-gcw0-toolchain.2014-08-20.tar.bz2-C /opt)
 else
   echo "Using cached toolchain directory."
 fi
--- build/travis-ci/install-sdl2.sh
+++ build/travis-ci/install-sdl2.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 set -e
-export SDL_PREFIX=$HOME/sdl2
-# check to see if sdl2 folder is empty
-if [ ! -d "$SDL_PREFIX" ]; then
-  curl -O http://www.libsdl.org/release/SDL2-2.0.4.tar.gz
-  tar -xzvf SDL2-2.0.4.tar.gz
-  (cd SDL2-2.0.4 && ./configure --prefix=$SDL_PREFIX && make && make install)
-  curl -O https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.0.tar.gz
-  tar -xzvf SDL2_image-2.0.0.tar.gz
-  (cd SDL2_image-2.0.0 && ./configure --prefix=$SDL_PREFIX && make && make install)
-  curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz
-  tar -xzvf SDL2_mixer-2.0.0.tar.gz
-  (cd SDL2_mixer-2.0.0 && ./configure --prefix=$SDL_PREFIX && make && make install)
-else
-  echo "Using cached sdl2 directory."
-fi
+curl -O http://www.libsdl.org/release/SDL2-2.0.4.tar.gz
+tar -xzvf SDL2-2.0.4.tar.gz
+(cd SDL2-2.0.4 && ./configure && make && sudo make install)
+curl -O https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz
+tar -xzvf SDL2_image-2.0.1.tar.gz
+(cd SDL2_image-2.0.1 && ./configure && make && sudo make install)
+curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.tar.gz
+tar -xzvf SDL2_mixer-2.0.1.tar.gz
+(cd SDL2_mixer-2.0.1 && ./configure && make && sudo make install)
