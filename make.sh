ba355c0c5f9bdee94889489a097e0f48d830ef1c
cxong <congusbongus@gmail.com>
2016-02-16 12:02:14 +0000

    Optionally use ninja to build with `make.sh`

--- make.sh
+++ make.sh
@@ -4,8 +4,15 @@
 
 UNAME_SHORT=`uname -s | cut -c1-5`
 if [ "$UNAME_SHORT" = "MINGW" ] ; then
+	echo "Build for MinGW"
 	cmake -G"MinGW Makefiles" .
+	make
+elif command -v ninja > /dev/null 2>&1; then
+	echo "Build using Ninja"
+	cmake -GNinja .
+	ninja
 else
+	echo "Build using default makefiles"
 	cmake .
+	make
 fi
-make
