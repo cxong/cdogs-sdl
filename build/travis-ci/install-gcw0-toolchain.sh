9a82db0c5cb523424c4b82cd3c98544779332d57
Cong <congusbongus@gmail.com>
2016-02-17 14:12:36 +1100

    Revert

--- build/travis-ci/install-gcw0-toolchain.sh
+++ build/travis-ci/install-gcw0-toolchain.sh
@@ -5,7 +5,7 @@ if [ ! -d "/opt/gcw0-toolchain" ]; then
   if [ ! -f "opendingux-gcw0-toolchain.2014-08-20.tar.bz2" ]; then
     curl -O http://www.gcw-zero.com/files/opendingux-gcw0-toolchain.2014-08-20.tar.bz2
   fi
-  (mkdir -p /opt && tar jxf opendingux-gcw0-toolchain.2014-08-20.tar.bz2 -C /opt)
+  (sudo mkdir -p /opt && sudo tar jxf opendingux-gcw0-toolchain.2014-08-20.tar.bz2 -C /opt)
 else
   echo "Using cached toolchain directory."
 fi
