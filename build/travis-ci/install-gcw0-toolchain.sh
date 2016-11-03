dadb4d1ef67c5ab937d37addee39dc2a481e0415
V.Barkov <neverscaired@gmail.com>
2016-11-03 18:44:51 +0300

    Trying to forcely get gcw0-toolchain

--- build/travis-ci/install-gcw0-toolchain.sh
+++ build/travis-ci/install-gcw0-toolchain.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
+
+if [[ "$1" == "--force" ]]
+then
+FORCE_INSTALL=true
+fi
+
 # check to see if toolchain folder is empty
-if [ ! -d "/opt/gcw0-toolchain" ]; then
+if [[ ! -d "/opt/gcw0-toolchain" || "$FORCE_INSTALL" == "true" ]]; then
   if [ ! -f "opendingux-gcw0-toolchain.2014-08-20.tar.bz2" ]; then
     curl -O http://www.gcw-zero.com/files/opendingux-gcw0-toolchain.2014-08-20.tar.bz2
   fi
