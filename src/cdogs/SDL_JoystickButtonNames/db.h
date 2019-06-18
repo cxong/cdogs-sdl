7b46eabf642072e846525bc33d91b29986465e1f
Cong Xu <congusbongus@gmail.com>
2019-06-18 22:47:31 +1000

    Add PS4/macos controller

--- src/cdogs/SDL_JoystickButtonNames/db.h
+++ src/cdogs/SDL_JoystickButtonNames/db.h
@@ -7,6 +7,7 @@ const char *db = "# Columns are: GUID, Joystick Name, button:name:r:g:b (repeate
 \n\
 # OS X\n\
 6d0400000000000016c2000000000000,Logitech Dual Action,a:2:244:244:244,b:3:244:244:244,back:9:64:64:64,dpdown::64:64:64,dpleft::64:64:64,dpright::64:64:64,dpup::64:64:64,leftshoulder:5:64:64:64,leftstick::64:64:64,lefttrigger:7:64:64:64,leftx::64:64:64,lefty::64:64:64,rightshoulder:6:64:64:64,rightstick::64:64:64,righttrigger:8:64:64:64,rightx::64:64:64,righty::64:64:64,start:10:64:64:64,x:1:244:244:244,y:4:244:244:244,platform:Mac OS X,\n\
+030000004c050000cc09000000016800,PS4 Controller,a:X:140:182:228,b:O:248:114:125,back:SHARE:60:60:60,guide:PS:222:233:240,dpdown::60:60:60,dpleft::60:60:60,dpright::60:60:60,dpup::60:60:60,leftshoulder::60:60:60,leftstick::60:60:60,lefttrigger::60:60:60,leftx::60:60:60,lefty::60:60:60,rightshoulder::60:60:60,rightstick::60:60:60,righttrigger::60:60:60,rightx::60:60:60,righty::60:60:60,start:OPTIONS:60:60:60,x:square:211:166:211,y:triangle:0:213:219,platform:Mac OS X\n\
 5e040000000000008e02000000000000,Xbox One Wired Controller,a::64:160:64,b::192:32:40,back:View:8:8:8,guide:Xbox:255:255:255,dpdown::8:8:8,dpleft::8:8:8,dpright::8:8:8,dpup::8:8:8,leftshoulder::8:8:8,leftstick::8:8:8,lefttrigger::8:8:8,leftx::8:8:8,lefty::8:8:8,rightshoulder::8:8:8,rightstick::8:8:8,righttrigger::8:8:8,rightx::8:8:8,righty::8:8:8,start:Menu:8:8:8,x::0:128:192,y::240:208:40,platform:Mac OS X\n\
 \n\
 # Linux\n\
