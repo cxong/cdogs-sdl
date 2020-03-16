daebf246328f3123df0c608980304134886f601f
Cong <congusbongus@gmail.com>
2020-03-16 22:22:28 +1100

    Metal and station tiles

--- graphics/wall/generate.ps1
+++ graphics/wall/generate.ps1
@@ -1,25 +1,29 @@
 # Use this script to generate all the other versions of wall tiles, given a complete "o" tile
 # Use "plasteel" as a template
 param (
-    [string]$name
+    [string]$name,
+    [int]$m = 1
 )
+$w = 16 - $m
+$h = 12 - $m
+$h2 = 24 - $m
 # NESW
-magick $name/o.png -crop 16x11+0+0 -colorspace sRGB -gravity North -define distort:viewport=16x12-0-0 -filter point -distort SRT 0 +repage $name/n.png
-magick $name/o.png -crop 16x23+0+1 -colorspace sRGB -gravity South -define distort:viewport=16x24-0-1 -filter point -distort SRT 0 +repage $name/s.png
-magick $name/o.png -crop 15x24+1+0 -colorspace sRGB -gravity East -define distort:viewport=16x24-1-0 -filter point -distort SRT 0 +repage $name/e.png
-magick $name/o.png -crop 15x24+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x24-0-0 -filter point -distort SRT 0 +repage $name/w.png
+Invoke-Expression ("magick $name/o.png -crop 16x{0}+0+0 -colorspace sRGB -gravity North -define distort:viewport=16x12-0-0 -filter point -distort SRT 0 +repage $name/n.png" -f $h)
+Invoke-Expression ("magick $name/o.png -crop 16x{0}+0+{1} -colorspace sRGB -gravity South -define distort:viewport=16x24-0-{1} -filter point -distort SRT 0 +repage $name/s.png" -f $h2,$m)
+Invoke-Expression ("magick $name/o.png -crop {0}x24+{1}+0 -colorspace sRGB -gravity East -define distort:viewport=16x24-{1}-0 -filter point -distort SRT 0 +repage $name/e.png" -f $w,$m)
+Invoke-Expression ("magick $name/o.png -crop {0}x24+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x24-0-0 -filter point -distort SRT 0 +repage $name/w.png" -f $w)
 # NW, NE, SW, SE
-magick $name/n.png -crop 15x12+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x12-0-0 -filter point -distort SRT 0 +repage $name/nw.png
-magick $name/n.png -crop 15x12+1+0 -colorspace sRGB -gravity East -define distort:viewport=16x12-1-0 -filter point -distort SRT 0 +repage $name/ne.png
-magick $name/s.png -crop 15x24+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x24-0-0 -filter point -distort SRT 0 +repage $name/sw.png
-magick $name/s.png -crop 15x24+1+0 -colorspace sRGB -gravity East -define distort:viewport=16x24-1-0 -filter point -distort SRT 0 +repage $name/se.png
+Invoke-Expression ("magick $name/n.png -crop {0}x12+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x12-0-0 -filter point -distort SRT 0 +repage $name/nw.png" -f $w)
+Invoke-Expression ("magick $name/n.png -crop {0}x12+{1}+0 -colorspace sRGB -gravity East -define distort:viewport=16x12-{1}-0 -filter point -distort SRT 0 +repage $name/ne.png" -f $w,$m)
+Invoke-Expression ("magick $name/s.png -crop {0}x24+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x24-0-0 -filter point -distort SRT 0 +repage $name/sw.png" -f $w)
+Invoke-Expression ("magick $name/s.png -crop {0}x24+{1}+0 -colorspace sRGB -gravity East -define distort:viewport=16x24-{1}-0 -filter point -distort SRT 0 +repage $name/se.png" -f $w,$m)
 # NT, ET, ST, WT
-magick $name/nw.png -crop 15x12+1+0 -colorspace sRGB -gravity East -define distort:viewport=16x12-1-0 -filter point -distort SRT 0 +repage $name/nt.png
-magick $name/ne.png -crop 16x11+0+1 -colorspace sRGB -gravity South -define distort:viewport=16x12-0-1 -filter point -distort SRT 0 +repage $name/et.png
-magick $name/se.png -crop 15x24+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x24-0-0 -filter point -distort SRT 0 +repage $name/st.png
-magick $name/sw.png -crop 16x11+0+0 -colorspace sRGB -gravity North -define distort:viewport=16x12-0-0 -filter point -distort SRT 0 +repage $name/wt.png
+Invoke-Expression ("magick $name/nw.png -crop {0}x12+{1}+0 -colorspace sRGB -gravity East -define distort:viewport=16x12-{1}-0 -filter point -distort SRT 0 +repage $name/nt.png" -f $w,$m)
+Invoke-Expression ("magick $name/ne.png -crop 16x{1}+0+{2} -colorspace sRGB -gravity South -define distort:viewport=16x12-0-{2} -filter point -distort SRT 0 +repage $name/et.png" -f $w,$h,$m)
+Invoke-Expression ("magick $name/se.png -crop {0}x24+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x24-0-0 -filter point -distort SRT 0 +repage $name/st.png" -f $w)
+Invoke-Expression ("magick $name/sw.png -crop 16x{1}+0+0 -colorspace sRGB -gravity North -define distort:viewport=16x12-0-0 -filter point -distort SRT 0 +repage $name/wt.png" -f $w,$h)
 # VH
-magick $name/n.png -crop 16x11+0+1 -colorspace sRGB -gravity South -define distort:viewport=16x12-0-1 -filter point -distort SRT 0 +repage $name/v.png
-magick $name/e.png -crop 15x24+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x24-0-0 -filter point -distort SRT 0 +repage $name/h.png
+Invoke-Expression ("magick $name/n.png -crop 16x{1}+0+{2} -colorspace sRGB -gravity South -define distort:viewport=16x12-0-{2} -filter point -distort SRT 0 +repage $name/v.png" -f $w,$h,$m)
+Invoke-Expression ("magick $name/e.png -crop {0}x24+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x24-0-0 -filter point -distort SRT 0 +repage $name/h.png" -f $w)
 # X
-magick $name/et.png -crop 15x12+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x12-0-0 -filter point -distort SRT 0 +repage $name/x.png
+Invoke-Expression ("magick $name/et.png -crop {0}x12+0+0 -colorspace sRGB -gravity West -define distort:viewport=16x12-0-0 -filter point -distort SRT 0 +repage $name/x.png" -f $w)
