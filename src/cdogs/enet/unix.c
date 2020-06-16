d2c62115a5f3ab1feb3da429ad55873529706173
Steve Wills <steve@mouf.net>
2020-06-16 09:58:25 -0400

    fix build on FreeBSD

--- src/cdogs/enet/unix.c
+++ src/cdogs/enet/unix.c
@@ -300,7 +300,7 @@ enet_socket_listen (ENetSocket socket, int backlog)
 ENetSocket
 enet_socket_create (ENetSocketType type)
 {
-    return socket (PF_INET, type == ENET_SOCKET_TYPE_DATAGRAM ? SOCK_DGRAM : SOCK_STREAM, 0);
+    return socket (AF_INET, type == ENET_SOCKET_TYPE_DATAGRAM ? SOCK_DGRAM : SOCK_STREAM, 0);
 }
 
 int
