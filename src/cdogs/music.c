62621e5bd3194bae90cbbbbdc7371d77ee97eeb0
Cong <congusbongus@gmail.com>
2021-04-18 11:44:54 +1000

    Don't replay music when changing volume

--- src/cdogs/music.c
+++ src/cdogs/music.c
@@ -2,7 +2,7 @@
     C-Dogs SDL
     A port of the legendary (and fun) action/arcade cdogs.
 
-    Copyright (c) 2013-2016, 2019 Cong Xu
+    Copyright (c) 2013-2016, 2019, 2021 Cong Xu
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -181,7 +181,7 @@ void MusicResume(SoundDevice *device)
 	{
 		Mix_ResumeMusic();
 	}
-	else
+	else if (!Mix_PlayingMusic())
 	{
 		Mix_PlayMusic(device->music, -1);
 	}
