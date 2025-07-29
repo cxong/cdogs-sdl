4a2269c8bbe44e9f8fc357c10222c9c0b54b9b5b
congusbongus <congusbongus@gmail.com>
2025-07-29 23:34:04 +1000

    Fix n3d music defs #712

--- src/cdogs/cwolfmap/audion3d.h
+++ src/cdogs/cwolfmap/audion3d.h
@@ -12,15 +12,15 @@
 
 #define LASTSOUND 43
 
-#define SONG1_MUS 0
+#define INTRO_MUS 0
 #define ALLGOOD_MUS 1
 #define SONG3_MUS 2
 #define SONG4_MUS 3
 #define FEEDTIME_MUS 4
 #define SONG6_MUS 5
-#define SONG7_MUS 6
+#define BOSS_MUS 6
 #define SONG8_MUS 7
-#define SONG9_MUS 8
+#define MENU_MUS 8
 #define HAPPYSNG_MUS 9
-#define SONG11_MUS 10
+#define HIGHSCORE_MUS 10
 #define LASTMUSIC 11
